5d8652e2a851ef18c47752f55185654ee8cbeeae
Olesia Opiakina <olesya.lo2017@yandex.ru>
2025-05-20 15:57:51 +0200

    Add abbility to remove cards

--- scripts/index.js
+++ scripts/index.js
@@ -9,10 +9,18 @@ function createCard(card) {
   const cardElement = template.querySelector(".places__item").cloneNode(true);
   cardElement.querySelector(".card__title").textContent = card.name;
   cardElement.querySelector(".card__image").src = card.link;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
   return cardElement;
 }
 
 // @todo: Функция удаления карточки
+
+function deleteCard(event) {
+  const cardElement = event.target.closest(".places__item");
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
 
